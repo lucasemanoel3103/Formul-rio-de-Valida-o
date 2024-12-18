1d5f132357a6b3c8c7d030b68d3da899faec3e9c
lucasemanoel3103 <160548558+lucasemanoel3103@users.noreply.github.com>
2024-12-17 21:49:13 -0300

    Andamento do projeto, html e css já projetados, no momento projetando o javascript

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+const form = document.querySelector("#form")
+const nameInput = document.querySelector("#name")
+const emailInput = document.querySelector("#email")
+const passwordInput = document.querySelector("#password")
+const jobSelectInput = document.querySelector("#job")
+const messageTextarea = document.querySelector("#message")
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+   
+})
